605bc32a352feab2d35f777962d8ad839887ab21
NickArthur <nickarthur.night@gmail.com>
2019-03-09 20:02:38 -0600

    Initial Commit

---
+++ MyTodosAppSyncApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTodosAppSyncApp
+//
+//  Created by Larry Nickerson on 3/9/19.
+//  Copyright © 2019 Lawrence Nickerson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
