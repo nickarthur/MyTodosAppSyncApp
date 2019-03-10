974922c6ec550f436afbd68f57934dda091cd161
NickArthur <nickarthur.night@gmail.com>
2019-03-09 21:36:53 -0600

    created an app sync client in app delegate

--- MyTodosAppSyncApp/AppDelegate.swift
+++ MyTodosAppSyncApp/AppDelegate.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import AWSAppSync
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    var appSyncClient: AWSAppSyncClient?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        do {
+            // You can choose the directory in which AppSync stores its persistent cache databases
+            let cacheConfiguration = try AWSAppSyncCacheConfiguration()
+            
+            // AppSync configuration & client initialization
+            let appSyncServiceConfig = try AWSAppSyncServiceConfig()
+            let appSyncConfig = try AWSAppSyncClientConfiguration(appSyncServiceConfig: appSyncServiceConfig,
+                                                                  cacheConfiguration: cacheConfiguration)
+            appSyncClient = try AWSAppSyncClient(appSyncConfig: appSyncConfig)
+            // Set id as the cache key for objects. See architecture section for details
+            appSyncClient?.apolloClient?.cacheKeyForObject = { $0["id"] }
+        } catch {
+            print("Error initializing appsync client. \(error)")
+        }
+        // other methods
         return true
     }
 
